44dcc0493447ede17665d9594fa3ba555b65b17b
ahmetaydogn <aydogan60ahmet@gmail.com>
2024-04-01 00:26:09 +0300

    Add project files.

---
+++ Core/DataAccess/EntityFramework/EfEntityRepositoryBase.cs
@@ -0,0 +1,48 @@
+﻿using Core.Entities;
+using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+
+namespace Core.DataAccess.EntityFramework;
+
+public class EfEntityRepositoryBase<TContext, TEntity> : IEntityRepository<TEntity>
+    where TContext : DbContext, new()
+    where TEntity : class, IEntity, new()
+
+{
+    public void Add(TEntity entity)
+    {
+        using (TContext context = new TContext())
+        {
+            var addedEntity = context.Entry(entity);
+            addedEntity.State = EntityState.Added;
+        }
+    }
+
+    public void Update(TEntity entity)
+    {
+        using (TContext context = new TContext())
+        {
+            var updatedEntity = context.Entry(entity);
+            updatedEntity.State = EntityState.Modified;
+        }
+    }
+
+    public void Delete(TEntity entity)
+    {
+        using (TContext context = new TContext())
+        {
+            var deletedEntity = context.Entry(entity);
+            deletedEntity.State = EntityState.Deleted;
+        }
+    }
+
+    public TEntity Get(Expression<Func<TEntity, bool>> filter)
+    {
+        throw new NotImplementedException();
+    }
+
+    public IList<TEntity> GetAll(Expression<Func<TEntity, bool>> filter = null)
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ Entities/Concrete/Product.cs
@@ -0,0 +1,14 @@
+﻿using Core.Entities;
+
+namespace Entities.Concrete;
+
+public class Product : IEntity
+{
+    public int ProductId { get; set; }
+    public int CategoryId { get; set; }
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public decimal Price { get; set; }
+    public decimal Vat { get; set; }
+    public int Quantity { get; set; }
+}
