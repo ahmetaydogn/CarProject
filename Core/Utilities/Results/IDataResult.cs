1bba1735662f7283d38beed4462f0ac03dc08114
ahmetaydogn <aydogan60ahmet@gmail.com>
2024-04-01 00:45:05 +0300

    Results structere completed

---
+++ Core/Utilities/Results/IDataResult.cs
@@ -0,0 +1,8 @@
+﻿using Core.Entities;
+
+namespace Core.Utilities.Results;
+
+public interface IDataResult<out T> : IEntity
+{
+    public T Data { get; }
+}
---
+++ Core/Utilities/Results/IResult.cs
@@ -0,0 +1,8 @@
+﻿namespace Core.Utilities.Results;
+
+public interface IResult
+{
+    // Since the values will only be received in the constructor structure, the get blocks are sufficient.
+    public bool IsSuccess { get; }
+    public string Message { get; }
+}
