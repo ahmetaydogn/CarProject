aecc2b13c9f5cf18a4e28bc7630f2b829bb732b6
ahmetaydogn <aydogan60ahmet@gmail.com>
2024-04-15 02:34:47 +0300

    BaseDialogListForm added.

--- UI.Win/Forms/BaseForm/BaseDialogListForm.Designer.cs
+++ UI.Win/Forms/BaseForm/BaseDialogListForm.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace UI.Win.Forms.BaseForm
 {
-    partial class BaseEditForm
+    partial class BaseDialogListForm
     {
         /// <summary>
         /// Required designer variable.
@@ -28,17 +28,14 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(BaseEditForm));
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(BaseDialogListForm));
             ribbonControl1 = new DevExpress.XtraBars.Ribbon.RibbonControl();
-            btnAdd = new DevExpress.XtraBars.BarButtonItem();
-            btnSave = new DevExpress.XtraBars.BarButtonItem();
-            btnSaveAs = new DevExpress.XtraBars.BarButtonItem();
-            btnUndo = new DevExpress.XtraBars.BarButtonItem();
-            btnDelete = new DevExpress.XtraBars.BarButtonItem();
             btnExit = new DevExpress.XtraBars.BarButtonItem();
+            btnSelect = new DevExpress.XtraBars.BarButtonItem();
+            btnRefreshSource = new DevExpress.XtraBars.BarButtonItem();
+            btnAdd = new DevExpress.XtraBars.BarButtonItem();
             ribbonPage1 = new DevExpress.XtraBars.Ribbon.RibbonPage();
             ribbonPageGroup1 = new DevExpress.XtraBars.Ribbon.RibbonPageGroup();
-            btnSelect = new DevExpress.XtraBars.BarButtonItem();
             ((System.ComponentModel.ISupportInitialize)ribbonControl1).BeginInit();
             SuspendLayout();
             // 
@@ -47,9 +44,9 @@
             ribbonControl1.DrawGroupCaptions = DevExpress.Utils.DefaultBoolean.False;
             ribbonControl1.DrawGroupsBorderMode = DevExpress.Utils.DefaultBoolean.False;
             ribbonControl1.ExpandCollapseItem.Id = 0;
-            ribbonControl1.Items.AddRange(new DevExpress.XtraBars.BarItem[] { ribbonControl1.ExpandCollapseItem, btnAdd, btnSave, btnSaveAs, btnUndo, btnDelete, btnExit, btnSelect });
+            ribbonControl1.Items.AddRange(new DevExpress.XtraBars.BarItem[] { ribbonControl1.ExpandCollapseItem, btnExit, btnSelect, btnRefreshSource, btnAdd });
             ribbonControl1.Location = new Point(0, 0);
-            ribbonControl1.MaxItemId = 8;
+            ribbonControl1.MaxItemId = 12;
             ribbonControl1.Name = "ribbonControl1";
             ribbonControl1.Pages.AddRange(new DevExpress.XtraBars.Ribbon.RibbonPage[] { ribbonPage1 });
             ribbonControl1.ShowApplicationButton = DevExpress.Utils.DefaultBoolean.False;
@@ -59,44 +56,9 @@
             ribbonControl1.ShowPageHeadersMode = DevExpress.XtraBars.Ribbon.ShowPageHeadersMode.Hide;
             ribbonControl1.ShowQatLocationSelector = false;
             ribbonControl1.ShowToolbarCustomizeItem = false;
-            ribbonControl1.Size = new Size(660, 126);
+            ribbonControl1.Size = new Size(1232, 126);
             ribbonControl1.Toolbar.ShowCustomizeItem = false;
             ribbonControl1.ToolbarLocation = DevExpress.XtraBars.Ribbon.RibbonQuickAccessToolbarLocation.Hidden;
-            // 
-            // btnAdd
-            // 
-            btnAdd.Caption = "Yeni";
-            btnAdd.Id = 1;
-            btnAdd.ImageOptions.SvgImage = (DevExpress.Utils.Svg.SvgImage)resources.GetObject("btnAdd.ImageOptions.SvgImage");
-            btnAdd.Name = "btnAdd";
-            // 
-            // btnSave
-            // 
-            btnSave.Caption = "Kaydet";
-            btnSave.Id = 2;
-            btnSave.ImageOptions.SvgImage = (DevExpress.Utils.Svg.SvgImage)resources.GetObject("btnSave.ImageOptions.SvgImage");
-            btnSave.Name = "btnSave";
-            // 
-            // btnSaveAs
-            // 
-            btnSaveAs.Caption = "Farklı Kaydet";
-            btnSaveAs.Id = 3;
-            btnSaveAs.ImageOptions.SvgImage = (DevExpress.Utils.Svg.SvgImage)resources.GetObject("btnSaveAs.ImageOptions.SvgImage");
-            btnSaveAs.Name = "btnSaveAs";
-            // 
-            // btnUndo
-            // 
-            btnUndo.Caption = "Geri Al";
-            btnUndo.Id = 4;
-            btnUndo.ImageOptions.SvgImage = (DevExpress.Utils.Svg.SvgImage)resources.GetObject("btnUndo.ImageOptions.SvgImage");
-            btnUndo.Name = "btnUndo";
-            // 
-            // btnDelete
-            // 
-            btnDelete.Caption = "Sil";
-            btnDelete.Id = 5;
-            btnDelete.ImageOptions.SvgImage = (DevExpress.Utils.Svg.SvgImage)resources.GetObject("btnDelete.ImageOptions.SvgImage");
-            btnDelete.Name = "btnDelete";
             // 
             // btnExit
             // 
@@ -104,6 +66,27 @@
             btnExit.Id = 6;
             btnExit.ImageOptions.SvgImage = (DevExpress.Utils.Svg.SvgImage)resources.GetObject("btnExit.ImageOptions.SvgImage");
             btnExit.Name = "btnExit";
+            // 
+            // btnSelect
+            // 
+            btnSelect.Caption = "Seç";
+            btnSelect.Id = 8;
+            btnSelect.ImageOptions.SvgImage = (DevExpress.Utils.Svg.SvgImage)resources.GetObject("btnSelect.ImageOptions.SvgImage");
+            btnSelect.Name = "btnSelect";
+            // 
+            // btnRefreshSource
+            // 
+            btnRefreshSource.Caption = "Yenile";
+            btnRefreshSource.Id = 10;
+            btnRefreshSource.ImageOptions.SvgImage = (DevExpress.Utils.Svg.SvgImage)resources.GetObject("btnRefreshSource.ImageOptions.SvgImage");
+            btnRefreshSource.Name = "btnRefreshSource";
+            // 
+            // btnAdd
+            // 
+            btnAdd.Caption = "Ekle";
+            btnAdd.Id = 11;
+            btnAdd.ImageOptions.SvgImage = (DevExpress.Utils.Svg.SvgImage)resources.GetObject("btnAdd.ImageOptions.SvgImage");
+            btnAdd.Name = "btnAdd";
             // 
             // ribbonPage1
             // 
@@ -113,48 +96,33 @@
             // 
             // ribbonPageGroup1
             // 
+            ribbonPageGroup1.ItemLinks.Add(btnSelect);
             ribbonPageGroup1.ItemLinks.Add(btnAdd);
-            ribbonPageGroup1.ItemLinks.Add(btnSave);
-            ribbonPageGroup1.ItemLinks.Add(btnSaveAs);
-            ribbonPageGroup1.ItemLinks.Add(btnUndo);
-            ribbonPageGroup1.ItemLinks.Add(btnDelete);
+            ribbonPageGroup1.ItemLinks.Add(btnRefreshSource);
             ribbonPageGroup1.ItemLinks.Add(btnExit);
             ribbonPageGroup1.Name = "ribbonPageGroup1";
             ribbonPageGroup1.Text = "ribbonPageGroup1";
             // 
-            // btnSelect
-            // 
-            btnSelect.Caption = "Seç";
-            btnSelect.Id = 7;
-            btnSelect.ImageOptions.SvgImage = (DevExpress.Utils.Svg.SvgImage)resources.GetObject("btnSelect.ImageOptions.SvgImage");
-            btnSelect.Name = "btnSelect";
-            // 
-            // BaseEditForm
+            // BaseDialogListForm
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(660, 461);
+            ClientSize = new Size(1232, 703);
             Controls.Add(ribbonControl1);
-            MinimizeBox = false;
-            Name = "BaseEditForm";
-            ShowIcon = false;
-            ShowInTaskbar = false;
-            StartPosition = FormStartPosition.CenterScreen;
+            Name = "BaseDialogListForm";
             ((System.ComponentModel.ISupportInitialize)ribbonControl1).EndInit();
             ResumeLayout(false);
             PerformLayout();
         }
 
         #endregion
+
+        protected internal DevExpress.XtraBars.Ribbon.RibbonControl ribbonControl1;
+        private DevExpress.XtraBars.BarButtonItem btnExit;
+        private DevExpress.XtraBars.BarButtonItem btnSelect;
         private DevExpress.XtraBars.Ribbon.RibbonPage ribbonPage1;
         private DevExpress.XtraBars.Ribbon.RibbonPageGroup ribbonPageGroup1;
+        private DevExpress.XtraBars.BarButtonItem btnRefreshSource;
         private DevExpress.XtraBars.BarButtonItem btnAdd;
-        private DevExpress.XtraBars.BarButtonItem btnSave;
-        private DevExpress.XtraBars.BarButtonItem btnSaveAs;
-        private DevExpress.XtraBars.BarButtonItem btnUndo;
-        private DevExpress.XtraBars.BarButtonItem btnDelete;
-        private DevExpress.XtraBars.BarButtonItem btnExit;
-        protected internal DevExpress.XtraBars.Ribbon.RibbonControl ribbonControl1;
-        private DevExpress.XtraBars.BarButtonItem btnSelect;
     }
 }
---
+++ UI.Win/Forms/BaseForm/BaseDialogListForm.cs
@@ -0,0 +1,48 @@
+﻿using DevExpress.XtraBars;
+
+namespace UI.Win.Forms.BaseForm;
+
+public partial class BaseDialogListForm : Form
+{
+    public BaseDialogListForm()
+    {
+        InitializeComponent();
+        EventsLoad();
+    }
+
+    private void EventsLoad()
+    {
+        foreach (var item in ribbonControl1.Items)
+        {
+            switch (item)
+            {
+                case BarButtonItem btn:
+                    btn.ItemClick += ButtonsClick;
+                    break;
+            }
+        }
+    }
+
+    private void ButtonsClick(object sender, ItemClickEventArgs e)
+    {
+        if (e.Item == btnSelect)
+            SelectEntity();
+        else if (e.Item == btnAdd)
+            AddEntity();
+        else if (e.Item == btnRefreshSource)
+            RefreshGridControl();
+        else if (e.Item == btnExit)
+            Close();
+    }
+
+    public virtual void SelectEntity() { }
+
+    public virtual void RefreshGridControl() { }
+
+    public virtual void AddEntity() { }
+
+    public void HideSelectButton ()
+    {
+        //btnSelect.Visibility = BarItemVisibility.Never;
+    }
+}
