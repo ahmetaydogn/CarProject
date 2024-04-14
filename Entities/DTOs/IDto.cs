02364330c361419fecca7b4477b1c619e8dd5255
ahmetaydogn <aydogan60ahmet@gmail.com>
2024-04-14 04:00:40 +0300

    DTOs added

---
+++ Entities/DTOs/IDto.cs
@@ -0,0 +1,5 @@
+﻿namespace Entities.DTOs;
+
+public interface IDto
+{
+}
---
+++ Entities/DTOs/SaleDto.cs
@@ -0,0 +1,18 @@
+﻿namespace Entities.DTOs;
+
+public class SaleDto : IDto
+{
+    public int SaleId { get; set; }
+    public int CustomerId { get; set; }
+    public int ProductId { get; set; }
+    public string CustomerFullName { get; set; }
+    public string CustomerPhone { get; set; }
+    public string ProductName { get; set; }
+    public string ProductModel { get; set; }
+    public DateTime SaleDate { get; set; }
+    public short Quantity { get; set; }
+    public decimal Price { get; set; }
+    public decimal Profit { get; set; }
+    public string PaymentMethod { get; set; }
+    public string BillNumber { get; set; }
+}
