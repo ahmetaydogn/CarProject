4d7580ba635573208687a6ccefab18b3dbdff91b
ahmetaydogn <aydogan60ahmet@gmail.com>
2024-04-16 01:50:20 +0300

    Bill added to Context

--- DataAccess/Concrete/EntityFramework/Contexts/CarProjectContext.cs
+++ DataAccess/Concrete/EntityFramework/Contexts/CarProjectContext.cs
@@ -12,6 +12,7 @@ public class CarProjectContext : DbContext
     }
 
     public DbSet<Product> Products { get; set; }
+    public DbSet<SubProduct> SubProducts { get; set; }
     public DbSet<Customer> Customers { get; set; }
     public DbSet<Bill> Bills { get; set; }
     public DbSet<Sale> Sales { get; set; }
